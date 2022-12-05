8f056a9440067482352ed24d0201901639d12164
JordiManyer <j.manyer@hotmail.com>
2022-12-05 22:35:02 +1100

    Day5

--- src/Aoc22.jl
+++ src/Aoc22.jl
@@ -5,16 +5,19 @@ include("Day1.jl")
 include("Day2.jl")
 include("Day3.jl")
 include("Day4.jl")
+include("Day5.jl")
 
 using Aoc22.Helpers
 using Aoc22.Day1
 using Aoc22.Day2
 using Aoc22.Day3
 using Aoc22.Day4
+using Aoc22.Day5
 
 export day1
 export day2
 export day3
 export day4
+export day5
 
 end
